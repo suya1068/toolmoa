ac6ad3c351661b2cedd5043f9bb787f40e795fce
suya1068 <suya1068@naver.com>
2024-04-19 04:39:24 +0900

    업데이트 netlify 플러그인 netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
  
 export default defineConfig({
-  plugins: [remix() ],
+  plugins: [remix(), netlifyPlugin() ],
 });
